9b9217da66a33f273255ebc5ac54cc0e4fe8041d
umacaspac <umacaspac@vertere-gs.com>
2026-02-20 10:00:40 +0800

    feat: Updated SidebarShell and LoginPage, Added more shadcn components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import { AuthProvider } from "./auth/AuthContext";
 import AppRouter from "./router";
-import "./index.css"
+import "./index.css";
+
+// 1. Import your custom Toaster component
+// (Adjust the import path based on where you saved the file)
+import { Toaster } from "@/components/ui/sonner";
 
 export default function App() {
   return (
     <AuthProvider>
       <AppRouter />
+      <Toaster position="top-right" richColors closeButton />
     </AuthProvider>
   );
 }
